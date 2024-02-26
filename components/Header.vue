2847c6f11bd6138c3af5a23ba5bb18610d6937b1
Anjanips <143574069+Anjanips@users.noreply.github.com>
2024-02-26 16:01:10 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row p-3 bg-primary text-white">
+    <div class="col-lg-1">
+      <img src="~assets/img/logo smk.png" alt="logo SMK4">
+    </div>
+
+    <div class="col-lg-11">
+      <h1>Perpus Digital</h1>
+      <div>SMKN 4 Tasikmalaya</div>
+      <address>
+        Jl.Depok, Sukamenak, Purbaratu
+      </address>
+    </div>
+  </div>
+</template>
+
---
+++ pages/pengunjung/tambah.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center my-4">ISI BUKU KUNJUNGAN</h2>
+        <form>
+          <div class="mb-3">
+            <input type="text" class="form-control form-control-lg rounded-5" placeholder="NAMA...">
+          </div>
+          <div class="mb-3">
+            <select class="form-control form-control-lg form-select rounded-5">
+              <option value="">KATEGORI</option>
+              <option value="Siswa">Siswa</option>
+              <option value="Guru">Guru</option>
+              <option value="Staf">Staf</option>
+              <option value="Umum">Umum</option>
+            </select>
+          </div>
+              <div class="mb-3">
+                <select class="form-control form-control-lg form-select rounded-5">
+                  <option value="">KELAS</option>
+                  <option value="X">X</option>
+                  <option value="XI">XI</option>
+                  <option value="XII">XII</option>
+                </select>
+              </div>
+           <div class="mb-3">
+            <select class="form-control form-control-lg form-select rounded-5">
+              <option value="">KEPERLUAN</option>
+                  <option value="baca">Baca Buku</option>
+                  <option value="pinjam">Pinjam Buku</option>
+                  <option value="kembalikan">Kembalikan Buku</option>
+            </select>
+           </div> 
+           <button type="submit" class="btn btn-primary btn-lg rounded-5 px-5">KIRIM</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
